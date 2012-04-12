ae7d53067c052230a26256e6867ad38b96b31953
Chris Paraiso <cparaiso@unicon.net>
2012-04-11 17:38:52 -0700

    Added admin.rb and its skeleton.

---
+++ lib/hana/admin.rb
@@ -0,0 +1,12 @@
+#administer Hana
+class Admin < Thor
+  desc 'install', 'Install Hana for the first time.'
+  def install
+    
+  end
+  
+  desc 'projects', 'Open projects.yml in $editor.'
+  def projects
+    
+  end
+end
--- lib/hana/main.rb
+++ lib/hana/main.rb
@@ -3,6 +3,7 @@ require_relative "project"
 require_relative "database"
 require_relative "tomcat"
 require_relative "uportal"
+require_relative "admin"
 module Hana
   def self.const_missing(c)
     Object.const_get(c)
